1e9718fe38bcfc572538fbd88cd3fc84a96500a8
Stan Sabin <sabinstan19@gmail.com>
2026-03-23 21:23:52 +0200

    chor: trigger go app

--- apps/test-cicd-app/main.go
+++ apps/test-cicd-app/main.go
@@ -28,6 +28,11 @@ func main() {
 		_, _ = w.Write([]byte("ok"))
 	})
 
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+
 	addr := ":" + port
 	log.Printf("listening on %s", addr)
 	if err := http.ListenAndServe(addr, nil); err != nil {
