a2ce0fa960acb9e982e55f183dd59b941c47f580
Stan Sabin <sabinstan19@gmail.com>
2026-04-19 22:05:58 +0300

    chore: remove metallb and trivy-operator and fix vault kv mount

---
+++ platform/platform/platform-secrets/vault-prod/terraform/mounts.tf
@@ -0,0 +1,6 @@
+resource "vault_mount" "kvv2" {
+  path        = local.secret_mount_path
+  type        = "kv"
+  options     = { version = "2" }
+  description = "KV Version 2 secret engine mount"
+}
