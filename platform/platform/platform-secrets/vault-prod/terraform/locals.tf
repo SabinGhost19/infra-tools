300f66b91b0d9849132a37ffa3e1c6a79bc6a2d0
Stan Sabin <sabinstan19@gmail.com>
2026-04-19 21:49:56 +0300

    chore(flux): gate infra-tools-secrets on vault-prod and external-secrets health checks

--- platform/platform/platform-secrets/vault-prod/terraform/locals.tf
+++ platform/platform/platform-secrets/vault-prod/terraform/locals.tf
@@ -1,6 +1,6 @@
 locals {
   vault_addr           = "http://vault-prod.vault-prod.svc.cluster.local:8200"
-  vault_token          = "root"
+  vault_token          = "hvs.8mGiyJiMladDU0JwqWfRFPRf"
   secret_mount_path    = "secret"
   kubernetes_auth_path = "kubernetes"
   transit_mount_path   = "transit"
